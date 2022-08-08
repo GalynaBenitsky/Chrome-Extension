f82768e2498ab963675f04da27f752e19c29da52
GalynaM <matsygingalyna@gmail.com>
2022-08-08 16:55:39 -0400

    Fist commit

---
+++ index.js
@@ -0,0 +1,64 @@
+let inputBtn = document.getElementById("input-btn");
+let myLeads = [];
+
+const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
+//localStorage.setItem("myName", "Galyna Benitskyi");
+// let name = localStorage.getItem("myName");
+// console.log(name);
+//localStorage.clear();
+const tabs = [{ url: "https://www.linkedin.com/in/galynabenitskyi" }];
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
+tabBtn.addEventListener("click", function () {
+  //console.log(tabs[0].url);
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    let activeTab = tabs[0];
+    let ActiveTabId = activeTab.id;
+  });
+
+  myLeads.push(tabs[0].url);
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+
+  render(myLeads);
+});
+
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+
+  render(myLeads);
+});
+
+function render(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    // listItems +=
+    //   "<li><a target='_blank' href='" +
+    //   myLeads[i] +
+    //   "'>" +
+    //   myLeads[i] +
+    //   "</a></li>";
+    listItems += `
+    <li>
+      <a target='_blank' href='${leads[i]}'>${leads[i]}</a>
+    </li>`;
+    // const li = document.createElement("li");
+    // li.textContent = myLeads[i];
+    // ulEl.append(li);
+  }
+  ulEl.innerHTML = listItems;
+}
